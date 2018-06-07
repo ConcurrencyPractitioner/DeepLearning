cc8c6453ca05526da69bc60b8fbec3d54f86ccfa
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2018-06-07 09:10:04 -0700

    Update FeedFowardNeuralNetwork.h

--- FeedFowardNeuralNetwork.h
+++ FeedFowardNeuralNetwork.h
@@ -26,7 +26,7 @@ public:
 	void addLayer(int size, int weightCount, vector<vector<double> > weights);
 	vector<double> fowardPropagate(vector<double> inputs);
 	vector<vector<vector<double> > > calculateGradient(vector<vector<double> > inputs,
-													   vector<vector<double> > outputs);
+							   vector<vector<double> > outputs);
 	void backPropagate(vector<vector<double> > inputs, vector<vector<double> > outputs);
 	double activationFunction(double input);
 	double derivativeFunction(double input);
