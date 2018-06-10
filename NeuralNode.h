bd48ce123264d0677b3ceba80bf253aa9cd5c7bb
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2018-06-09 19:54:03 -0700

    Add files via upload

---
+++ NeuralNode.h
@@ -0,0 +1,37 @@
+/*
+ * NeuralNode.h
+ *
+ *  Created on: May 27, 2018
+ *      Author: richardyu
+ */
+
+#ifndef NEURALNODE_H_
+#define NEURALNODE_H_
+
+#include <vector>
+
+using namespace std;
+
+class NeuralNode {
+private:
+	double learningRate;
+public:
+	vector<double> weights;
+	NeuralNode();
+	NeuralNode(vector<double> modifiers, double rate);
+	virtual ~NeuralNode();
+	double computeResult(vector<double> inputs);
+	vector<double> calculateGradient(vector<double> inputs,
+			vector<vector<double> > matrix,
+			vector<double> output);
+	void train(vector<vector<double> > inputs, vector<double> outputs, double epochs);
+	void newtonTrain(vector<vector<double> > inputs, vector<double> output, int epochs);
+	static vector<vector<double> > computeHessian(vector<vector<double> > initHessian,
+			vector<double> xDelta,
+			vector<double> gradientDelta);
+	static double determineRate(vector<double> gradient,
+			vector<double> conjugateDirection,
+			vector<vector<double> > inputs);
+};
+
+#endif /* NEURALNODE_H_ */
